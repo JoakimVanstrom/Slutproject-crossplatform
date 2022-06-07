3c550e3f6450cdbcb013ab287b3bac2f0b78d503
99ru <yan.99ru@gmail.com>
2022-06-07 16:42:55 +0200

    changed timezone from Dublin to Stockholm

--- src/components/clock/LiveClock.jsx
+++ src/components/clock/LiveClock.jsx
@@ -1,26 +1,27 @@
 import AnalogClock from "ras-react-component";
 
-const style = {
-  numerals: "western_arabic",
-  showMinuteScale: true,
-  showHourScale: true,
-  showNumbers: true,
-  radialDirectionOfNumbers: false,
-  colorOfScalesAndNumbers: `black`,
-  hourHandColor: `#151515`,
-  minuteHandColor: `black`,
-  secondHandColor: `red`,
-  firstCircleColor: `white`,
-  secondCircleColor: `white`,
-  thirdCircleColor: `white`,
-  fourthCircleColor: `black`,
-  centerDotColor: `black`,
-  width: 300,
-  numberSize: 150,
-  iana: `Europe/Dublin`,
-};
-
 const LiveClock = () => {
+
+  const style = {
+    numerals: "western_arabic",
+    showMinuteScale: true,
+    showHourScale: true,
+    showNumbers: true,
+    radialDirectionOfNumbers: false,
+    colorOfScalesAndNumbers: `black`,
+    hourHandColor: `#151515`,
+    minuteHandColor: `black`,
+    secondHandColor: `#6c3454`,
+    firstCircleColor: `white`,
+    secondCircleColor: `white`,
+    thirdCircleColor: `white`,
+    fourthCircleColor: `black`,
+    centerDotColor: `black`,
+    width: 350,
+    numberSize: 120,
+    iana: `Europe/Stockholm`,
+  };
+
   return (
     <div>
       <AnalogClock characteristics={style} />

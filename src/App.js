e0133cfe5e031da21756ed3c6293e123c50bb14c
99ru <yan.99ru@gmail.com>
2022-06-07 13:52:36 +0200

    Header

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import './App.css';
+import Header from "./components/header/Header";
 import WeatherApp from './components/weather/WeatherApp';
 import LiveClock from './components/clock/LiveClock';
 import CalendarComp from './components/calendar/CalendarComp';
 
 
+
 function App() {
   return (
     <div className="App">
+      <Header />
       <LiveClock />
       <WeatherApp />
       <CalendarComp />
+     
     </div>
   );
 }

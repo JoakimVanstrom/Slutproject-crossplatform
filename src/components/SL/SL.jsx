9ad48bffe40980f678df1c097b4ad66eb3c33ed1
99ru <yan.99ru@gmail.com>
2022-06-09 18:31:40 +0200

    css + avgångar

--- src/components/SL/SL.jsx
+++ src/components/SL/SL.jsx
@@ -1,14 +1,17 @@
 import { useEffect, useState } from "react";
 import * as API from "./API";
 import "./SL.css";
+import Logo from "../../assets/images/logo.png";
 
 const SL = () => {
-  const [data, setData] = useState(null);
+  const [data, setData] = useState([]);
 
+  
   useEffect(() => {
     async function fetchData() {
-      const response = await API.getData();
-      setData(response);
+      const res = await API.getData();
+      setData(res.data.Departure);
+      console.log(res.data.Departure);
     }
     fetchData();
   }, []);
@@ -16,7 +19,22 @@ const SL = () => {
   return (
     <div className="App">
       <div className="reseplaneraren">
-        <h2 className="header">Avgångar</h2>
+
+        <div className="header">
+        <img src={Logo} alt="sl-logo" width={80} />
+        <h2>Avgångar</h2>
+        </div>
+        
+
+        <div className="traffic">
+          {data.map((item) => (
+            <ul className="traffic-box" key={item.JourneyDetailRef.ref}>
+              <strong> <li className="traffic-name">{item.name}</li> </strong>
+              <li className="traffic-time"> Avgång {item.time}</li>
+              <li className="traffic-direction"> mot {item.direction}</li>
+            </ul>
+          ))}
+        </div>
       </div>
     </div>
   );

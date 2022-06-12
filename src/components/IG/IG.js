e9ba01e15f4788518d3f04087ce32942ca0d92c4
Joakim Vanström <joakim.vanstrom@gmail.com>
2022-06-12 19:51:48 +0200

    commented out IG for netlify

--- src/components/IG/IG.js
+++ src/components/IG/IG.js
@@ -1,11 +1,11 @@
 
-const IG = () => {
-  const ig = require('instagram-scraping');
+// const IG = () => {
+//   const ig = require('instagram-scraping');
 
-  ig.scrapeTag('ithskorv').then((result) => {
-    console.dir(result);
-  });
-}
+//   ig.scrapeTag('ithskorv').then((result) => {
+//     console.dir(result);
+//   });
+// }
 
-export default IG;
+// export default IG;
 

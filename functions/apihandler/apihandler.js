1fdb5e45ef47aa8a63c3e87b417d959cb38a343b
99ru <yan.99ru@gmail.com>
2022-06-13 10:20:40 +0200

    test

--- functions/apihandler/apihandler.js
+++ functions/apihandler/apihandler.js
@@ -1,12 +1,12 @@
 // Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
 import fetch from "node-fetch";
 
-const token =
+/* const token =
   "EAAKJ8RQL5aEBADc622SZCpKrzE5qnhg9Iiyn226vtwHuReenZBD758N5ZBiRrWVErmMxNQJZBMdojjNQqQ5Cdi4XjTVwcMhVO6JhyFTxZCvIUl3ONw5vMWfOKpAZAInwt7OlYCY2Fd5JUKxQguzPpwTaxqYrkpwcSPyjXqkScIXX9JJoK4dU5asiqkwLyFGio9TfRZCQtpxfwZDZD";
 
-const URL = "https://graph.facebook.com/v14.0/me?fields=id%2Cname";
+const URL = "https://graph.facebook.com/v14.0/me?fields=id%2Cname"; */
 
-const handler = async (event) => {
+/* const handler = async (event) => {
   try {
     const response = await fetch(URL, {
       headers: {
@@ -15,23 +15,29 @@ const handler = async (event) => {
       },
     });
     const data = await response.json();
-    console.log(data);
-
-    const tweets = data.data.map((tweet) => {
-      const user = data.includes.users.find(
-        (user) => user.id === tweet.author_id
-      );
-      const { name, username } = user;
-      const { text } = tweet;
-      return { name, username, text };
-    });
     return {
       statusCode: 200,
-      body: JSON.stringify(tweets),
+      body: JSON.stringify(data),
     };
   } catch (err) {
     return { error: err.message };
   }
-};
+}; */
+
+const handler = async (event) => {
+  try {
+    const response = await fetch(
+      "https://swapi.dev/api/people/1"
+    );
+    const data = await response.json();
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data),
+    };
+  }
+  catch (err) {
+    return { error: err.message };
+  }
+}
 
 module.exports = { handler };

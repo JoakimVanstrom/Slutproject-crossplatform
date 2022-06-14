5f1a283c9bd60d28f24582eca0bb4efaf15e179c
Joakim Vanström <89384804+Psykisk@users.noreply.github.com>
2022-06-14 15:24:58 +0200

    end of schoolday

--- src/components/twitter/TwitterFeed.jsx
+++ src/components/twitter/TwitterFeed.jsx
@@ -1,33 +1,37 @@
 import { useState, useEffect } from "react";
-// import tweets from '../../assets/tweets.json';
+import tweets from '../../assets/tweets.json';
 import './twitter.css'
 
 function TwitterFeed() {
 
-    const [Tweets, setTweets] = useState([{name: "Loading...", username: "Loading...", text: "Loading..."}]);
-    // const [Tweets, setTweets] = useState(tweets);
+    // const [Tweets, setTweets] = useState([{name: "Loading...", username: "Loading...", text: "Loading..."}]);
+    const [Tweets, setTweets] = useState(tweets);
 
-    async function fetchNetlify() {
-        fetch('https://roaring-cactus-4a1a3c.netlify.app/.netlify/functions/apihandler')
-            .then(response => response.json())
-            .then(data => {
-                setTweets(data);
-                console.log(data)
-            })
-            .catch(error => console.log(error));
-    }
+    // http://localhost:8888/.netlify/functions/apihandler
+    // https://roaring-cactus-4a1a3c.netlify.app/.netlify/functions/apihandler
 
-    useEffect(() => {
-        setInterval(() => {
-            fetchNetlify();
-        }, 10000);
-    }, []);
+    // async function fetchNetlify() {
+    //     fetch('https://roaring-cactus-4a1a3c.netlify.app/.netlify/functions/apihandler')
+    //         .then(response => response.json())
+    //         .then(data => {
+    //             setTweets(data);
+    //             console.log(data)
+    //         })
+    //         .catch(error => console.log(error));
+    // }
+
+    // useEffect(() => {
+    //     setInterval(() => {
+    //         fetchNetlify();
+    //     }, 10000);
+    // }, []);
 
     return (
         <div className="TwitterWrapper">
             <div className="TweetHeader">
                 <h1>Tweets</h1>
             </div>
+
             {Tweets.map((tweet, index) => {
                 return (
                     <div className="Tweet" key={index}>
